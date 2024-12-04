4c30b005b37c14398c74a86ff5d9673036719f4e
Squirrelip <squirreliptech@gmail.com>
2024-12-04 04:25:24 -0500

    new codes

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# AI_based_abbreviation_expansion-
+# AI_based_abbreviation_expansion
+
+Dataset: https://github.com/McGill-NLP/medal?tab=readme-ov-file
--- download_augmented_clinical_notes.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-
-df = pd.read_json("hf://datasets/AGBonnet/augmented-clinical-notes/augmented_notes_30K.jsonl", lines=True)
-print(df.head())
